176f0563b270e6415797d67b71e46cfb47ce88ac
Brandon Walker <branlewalk@gmail.com>
2022-02-04 22:43:06 -0800

    Started working on recipes endpoint, added test.http to test endpoints

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import json
 from flask import Flask, jsonify, render_template
+import pymysql
+
+db = pymysql.connect(user='root', passwd='password', host='localhost', database='brewify_db')
+cursor = db.cursor()
 
 # Connection String 'mysql+pymysql://db/brew_project'
 app = Flask(__name__, static_url_path='')
@@ -9,9 +13,12 @@ app.url_map.strict_slashes = False
 def index():
     pass
 
-@app.route('/recipes')
+@app.route('/recipes', methods=['POST'])
 def recipes():
-    pass
+    sproc = "CreateRecipe"
+    params = ('HelloWorldRecipe', 'All-Grain', 10, 5.0, 2, 'First Recipe', 1, 1, None)
+    call_stored_procedure(sproc, params)
+    
 
 @app.route('/session')
 def session():
@@ -21,5 +28,11 @@ def session():
 def ingredients():
     pass
 
+def call_stored_procedure(sproc, params):
+    cursor.callproc(sproc, params) 
+    db.commit()
+    cursor.close()
+    db.close()
+
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=7099, host='0.0.0.0')
